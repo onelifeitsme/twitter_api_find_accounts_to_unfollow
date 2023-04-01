ebf45b3b34d8c32af987bbe87134ced9ec7d96aa
Artur Karpovich <onelifeitsme@gmail.com>
2023-04-01 13:00:43 +0300

    exception added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import requests
 import tweepy
+import time
 
 client = tweepy.Client('AAAAAAAAAAAAAAAAAAAAAAkebQEAAAAAmpzxzNABmgMUqsFytnKci%2BtI7jQ%3DEuphW1Hg5YtlCJS26xi8LjZDbuAGBT1RaeiFvPFgAbmUkACmll', wait_on_rate_limit=True)
 
@@ -42,15 +44,28 @@ users_to_unfollow = []
 
 
 for user in my_following_list:
-    print(f'Провермяем юзера {user.username}')
-    users_tweets = get_users_tweets(user)
-    for tweet in users_tweets:
-        if check_if_i_liked_tweet(tweet):
-            print(f'https://twitter.com/{user.username} проверку прошёл')
-            break
-        if tweet == users_tweets[-1] and not check_if_i_liked_tweet(tweet):
-            users_to_unfollow.append(f'https://twitter.com/{user.username}')
-            print(f'https://twitter.com/{user.username} проверку не прошёл')
+    try:
+        print(f'Провермяем юзера {user.username}')
+        users_tweets = get_users_tweets(user)
+        for tweet in users_tweets:
+            if check_if_i_liked_tweet(tweet):
+                print(f'https://twitter.com/{user.username} проверку прошёл')
+                break
+            if tweet == users_tweets[-1] and not check_if_i_liked_tweet(tweet):
+                users_to_unfollow.append(f'https://twitter.com/{user.username}')
+                print(f'https://twitter.com/{user.username} проверку не прошёл')
+    except requests.exceptions.ReadTimeout:
+        time.sleep(300)
+        print(f'Провермяем юзера {user.username}')
+        users_tweets = get_users_tweets(user)
+        for tweet in users_tweets:
+            if check_if_i_liked_tweet(tweet):
+                print(f'https://twitter.com/{user.username} проверку прошёл')
+                break
+            if tweet == users_tweets[-1] and not check_if_i_liked_tweet(tweet):
+                users_to_unfollow.append(f'https://twitter.com/{user.username}')
+                print(f'https://twitter.com/{user.username} проверку не прошёл')
+
 
 
 
